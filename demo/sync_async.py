74e897b183df4d0842dd0993da986f2ae354b969
chairco <chairco@gmail.com>
2018-06-05 14:49:34 +0800

    Modify some func name to easy understand

--- demo/sync_async.py
+++ demo/sync_async.py
@@ -4,100 +4,99 @@ import concurrent.futures
 
 from asgiref.sync import sync_to_async, async_to_sync
 
-# async to sync
+
+# async get_chat_id
 async def async_get_chat_id(name):
     start = time.time()
-    print('hello async, %.1f sec' % (time.time() - start))
+    print('hello async_get_chat_id, %.1f sec' % (time.time() - start))
     await asyncio.sleep(3)
-    print('hello async, %.1f sec' % (time.time() - start))
+    print('hello async_get_chat_id, %.1f sec' % (time.time() - start))
     return "chat-%s" % name
 
 
+# sync get_chat_id
 def get_chat_id(name):
     start = time.time()
-    print('hello block, %.1f sec' % (time.time() - start))
+    print('hello get_chat_id, %.1f sec' % (time.time() - start))
     time.sleep(3)
-    print('hello block, %.1f sec' % (time.time() - start))
+    print('hello get_chat_id, %.1f sec' % (time.time() - start))
     return "chat-%s" % name
 
 
-async def async_main():
+##### async to aysnc #####
+async def async_async_main():
     id_coroutine = async_get_chat_id("django")
     result = await id_coroutine
     print(result)
 
 
-async def main():
+##### sync to aysnc #####
+async def sync_async_main():
     result = get_chat_id("django")
 
 
-# sync to async
-async def main_sync_to_async():
+##### using asgiref#####
+async def asgiref_sync_to_async_main():
     result = await sync_to_async(get_chat_id)("django")
 
 
+##### using thread #####
 async def thread_main():
     #executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
     loop = asyncio.get_event_loop()
     #result = await loop.run_in_executor(executor, get_chat_id, "django")
+    
     # 'None' default run ThreadPoolExecutor 
     result = await loop.run_in_executor(None, get_chat_id, "django")
 
 
-async def get_chat_id_async_to_sync(name):
-    start = time.time()
-    print('hello block, %.1f sec' % (time.time() - start))
-    await asyncio.sleep(3)
-    print('hello block, %.1f sec' % (time.time() - start))
-    return "chat-%s" % name
-
-
-def main_async_to_sync():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    result = loop.run_until_complete(get_chat_id_async_to_sync("django"))
-    #result = async_to_sync(get_chat_id)("django")
-
-
-def noblock_add_async_to_sync():
-    result = async_to_sync(get_chat_id_async_to_sync)("django")
-
-
-# example of Sync from Asyc
-def noblock():
+##### main async to async #####
+def call_async_async():
     loop = asyncio.get_event_loop()
-    tasks = [async_main(), async_main()]
+    tasks = [async_async_main(), async_async_main()]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
 
 
-def block():
+##### main sync to async #####
+def call_sync_to_async():
     loop = asyncio.get_event_loop()
-    tasks = [main(), main()]
+    tasks = [sync_async_main(), sync_async_main()]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
 
 
-def block_add_sync_to_aync():
-    loop = asyncio.get_event_loop()
-    tasks = [main_sync_to_async(), main_sync_to_async()]
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
-
-
-def block_to_thread():
+def call_thread():
     loop = asyncio.get_event_loop()
     tasks = [thread_main(), thread_main()]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
 
 
+def call_asgiref_sync_to_aync():
+    loop = asyncio.get_event_loop()
+    tasks = [asgiref_sync_to_async_main(), asgiref_sync_to_async_main()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
 
-# noblock()
-# block()
 
-# block_to_thread()
-# block_add_sync_to_aync()
+##### main async to sync #####
+def call_createventloop_async_to_sync():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(async_get_chat_id("django"))
+    #result = async_to_sync(get_chat_id)("django")
 
-# main_async_to_sync()
-# noblock_add_async_to_sync()
+
+def call_asgiref_async_to_sync():
+    result = async_to_sync(async_get_chat_id)("django")
+
+
+# call_async_async()
+
+# call_sync_to_async()  # <-- has block
+# call_thread()
+# call_asgiref_sync_to_aync()
+
+# call_createventloop_async_to_sync()
+# call_asgiref_async_to_sync()
